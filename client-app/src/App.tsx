8782381a7b3f1309037fd5afb7664c4fd05a1256
Roshan Bassan <roshan.bassan@gmail.com>
2019-12-18 13:01:14 +1100

    Setting up Semantic UI

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,32 +1,36 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, { Component } from "react";
+// import "./App.css";
 import axios from "axios";
+import { Header, Icon, List } from "semantic-ui-react";
 
-class App extends Component  {
-  
+class App extends Component {
   state = {
     values: []
-  }
+  };
 
-  componentDidMount() { 
-    // rerender of component. 
-    axios.get("http://localhost:5000/api/values")
-    .then((response) => { 
-      console.log(response.data);
-      this.setState({values : response.data })
-    })
+  componentDidMount() {
+    // rerender of component.
+    axios.get("http://localhost:5000/api/values").then(response => {
+      //console.log(response);
+      this.setState({ values: response.data });
+    });
   }
 
   render() {
-  return (
-    <div className="App">
-      <ul>
-        { this.state.values.map((value: any) => (
-          <li key={value.id}>{value.name}</li>
-        ))}
-      </ul>
-    </div>
-  );
+    return (
+      <div className="App">
+        <Header as="h2">
+          <Icon name="users" />
+          <Header.Content>Reactivites</Header.Content>
+        </Header>
+
+        <List>
+          {this.state.values.map((value: any) => (
+            <List.Item key={value.id}>{value.name}</List.Item>
+          ))}
+        </List>
+      </div>
+    );
   }
 }
 
