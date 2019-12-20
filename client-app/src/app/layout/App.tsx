77fbaa2714ce35022c77a1e49da1eaf89202f2e4
Roshan Bassan <roshan.bassan@gmail.com>
2019-12-20 12:18:53 +1100

    Creating the Activity List

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -4,6 +4,7 @@ import { List, Container } from "semantic-ui-react";
 import { IActivity } from "../models/activity";
 import { NavBar } from "../../features/nav/NavBar";
 import "./styles.css";
+import { ActivityDashboard } from "../../features/activities/dashboard/ActivityDashboard";
 
 const App = () => {
   const [activities, setActivities] = useState<IActivity[]>([]);
@@ -23,11 +24,7 @@ const App = () => {
     <Fragment>
       <NavBar />
       <Container style={{ marginTop: "7em" }}>
-        <List>
-          {activities.map(activity => (
-            <List.Item key={activity.id}>{activity.title}</List.Item>
-          ))}
-        </List>
+        <ActivityDashboard activities={activities} />
       </Container>
     </Fragment>
   );
---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Grid, List } from "semantic-ui-react";
+import { IActivity } from "../../../app/models/activity";
+import { ActivityList } from "./ActivityList";
+
+interface IProps {
+  activities: IActivity[];
+}
+
+export const ActivityDashboard: React.FC<IProps> = ({ activities }) => {
+  return (
+    <Grid>
+      <Grid.Column width={10}>
+        <List>
+          <ActivityList activities={activities} />
+
+          {/* {activities.map(activity => (
+            <List.Item key={activity.id}>{activity.title}</List.Item>
+          ))} */}
+        </List>
+      </Grid.Column>
+    </Grid>
+  );
+};
