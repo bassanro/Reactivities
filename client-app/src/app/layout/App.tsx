741187ad03c8e3c5603ba5e24928e91fbcdfdf4e
Roshan Bassan <roshan.bassan@gmail.com>
2019-12-22 02:46:30 +1100

    Adding a loading component

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -5,6 +5,7 @@ import { NavBar } from "../../features/nav/NavBar";
 import "./styles.css";
 import { ActivityDashboard } from "../../features/activities/dashboard/ActivityDashboard";
 import agent from "../api/agent";
+import { LoadingComponent } from "./LoadingComponent";
 
 const App = () => {
   const [activities, setActivities] = useState<IActivity[]>([]);
@@ -12,6 +13,7 @@ const App = () => {
     null
   );
   const [editMode, setEditMode] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const handleOpenCreateForm = () => {
     setSelectedActivity(null);
@@ -50,18 +52,23 @@ const App = () => {
   };
 
   useEffect(() => {
-    agent.activities.list().then(response => {
-      //console.log(response);
-      let activities: IActivity[] = [];
-      response.forEach(activity => {
-        activity.date = activity.date.split(".")[0];
-        activities.push(activity);
-      });
-      setActivities(activities);
-    });
+    agent.activities
+      .list()
+      .then(response => {
+        //console.log(response);
+        let activities: IActivity[] = [];
+        response.forEach(activity => {
+          activity.date = activity.date.split(".")[0];
+          activities.push(activity);
+        });
+        setActivities(activities);
+      })
+      .then(() => setLoading(false));
     return () => {};
     // Empty array would ensure useEffect runs one time only.
   }, []);
+
+  if (loading) return <LoadingComponent content="Loading Activities .. " />;
 
   return (
     <Fragment>
