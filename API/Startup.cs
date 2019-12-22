b779f5aa10889004bf9fe95a8773ba140bed1f5e
Roshan Bassan <roshan.bassan@gmail.com>
2019-12-22 23:33:03 +1100

    Adding serverSide Validation - Fluent Validation

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,4 +1,5 @@
 using Application.Activities;
+using FluentValidation.AspNetCore;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -31,7 +32,11 @@ namespace API
                 policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
             }));
             services.AddMediatR(typeof(ListH.Handler).Assembly);
-            services.AddControllers();
+            services.AddControllers()
+            .AddFluentValidation(cfg =>
+            {
+                cfg.RegisterValidatorsFromAssemblyContaining<Create>();
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -4,6 +4,8 @@ using System.Threading.Tasks;
 using Domain;
 using MediatR;
 using Persistence;
+using System.ComponentModel.DataAnnotations;
+using FluentValidation;
 
 namespace Application.Activities
 {
@@ -25,6 +27,19 @@ namespace Application.Activities
             public string City { get; set; }
 
             public string Venue { get; set; }
+        }
+
+        public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(x => x.Title).NotEmpty();
+                RuleFor(x => x.Description).NotEmpty();
+                RuleFor(x => x.Category).NotEmpty();
+                RuleFor(x => x.Date).NotEmpty();
+                RuleFor(x => x.City).NotEmpty();
+                RuleFor(x => x.Venue).NotEmpty();
+            }
         }
 
         public class Hanlder : IRequestHandler<Command>
