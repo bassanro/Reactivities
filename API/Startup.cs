e6e8e35f349cc1308a6db7137acf78f47c6ac8a2
Roshan Bassan <roshan.bassan@gmail.com>
2020-02-07 13:34:59 +1100

    Seeding users to DB

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,9 +1,11 @@
 using API.Middleware;
 using Application.Activities;
+using Domain;
 using FluentValidation.AspNetCore;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,57 +14,65 @@ using Persistence;
 
 namespace API
 {
-  public class Startup
-  {
-    public Startup(IConfiguration configuration)
+    public class Startup
     {
-      Configuration = configuration;
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors(opt => opt.AddPolicy("CorsPolicy", policy =>
+            {
+                policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+            }));
+            services.AddMediatR(typeof(ListH.Handler).Assembly);
+            services.AddControllers()
+            .AddFluentValidation(cfg =>
+            {
+                cfg.RegisterValidatorsFromAssemblyContaining<Create>();
+            });
+
+            // Configure Identity. 
+            var builder = services.AddIdentityCore<AppUser>();
+            var identityBuilder = new IdentityBuilder(builder.UserType, builder.Services);
+            identityBuilder.AddEntityFrameworkStores<DataContext>();
+            identityBuilder.AddSignInManager<SignInManager<AppUser>>();
+
+            services.AddAuthentication();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseMiddleware<ErrorHandlingMiddleware>();
+            if (env.IsDevelopment())
+            {
+                // app.UseDeveloperExceptionPage();
+
+            }
+
+            // app.UseHttpsRedirection();
+
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseCors("CorsPolicy");
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
     }
-
-    public IConfiguration Configuration { get; }
-
-    // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services.AddDbContext<DataContext>(opt =>
-      {
-        opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
-      });
-      services.AddCors(opt => opt.AddPolicy("CorsPolicy", policy =>
-      {
-        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
-      }));
-      services.AddMediatR(typeof(ListH.Handler).Assembly);
-      services.AddControllers()
-      .AddFluentValidation(cfg =>
-      {
-        cfg.RegisterValidatorsFromAssemblyContaining<Create>();
-      });
-    }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-      app.UseMiddleware<ErrorHandlingMiddleware>();
-      if (env.IsDevelopment())
-      {
-        // app.UseDeveloperExceptionPage();
-
-      }
-
-      // app.UseHttpsRedirection();
-
-
-      app.UseRouting();
-
-      app.UseAuthorization();
-
-      app.UseCors("CorsPolicy");
-
-      app.UseEndpoints(endpoints =>
-      {
-        endpoints.MapControllers();
-      });
-    }
-  }
 }
